3ed0ebec3b85b53b8723d0097fe8488fb56d51cf
Jonathan Bowman <bowman@swiftbunny.com>
2020-11-14 16:24:10 -0500

    Init

---
+++ index.js
@@ -0,0 +1,24 @@
+var app = require("express")();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
+
+var users = {};
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", socket => {
+  users[socket.id] = "";
+  socket.on("chat message", payload => {
+    users[socket.id] = payload.user;
+    io.emit("chat message", payload);
+  });
+  socket.on("disconnect", () => {
+    delete users[socket.id];
+  });
+});
+
+http.listen(3000, () => {
+  console.log("listening on *:3000");
+});
